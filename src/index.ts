daac36d9cef9a06cb435d267c124c1e0d8b3b3ab
wrule <1982775886@qq.com>
2024-06-26 12:45:36 +0800

    基本完成

--- src/index.ts
+++ src/index.ts
@@ -2,15 +2,23 @@ import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export
-function hello() {
+function server(port: number) {
   const app = express();
   const proxyMiddleware = createProxyMiddleware({
-    target: 'http://10.10.200.21:8083',
+    target: `http://10.10.200.21:${port}/api`,
     changeOrigin: true,
     cookieDomainRewrite: '',
     headers: { Connection: 'keep-alive' },
   });
   app.use('/api', proxyMiddleware);
-  app.listen(9093);
-  console.log(9093, '...');
+  const port2 = port + 1000;
+  app.listen(port2);
+  console.log(port, port2, '...');
+}
+
+export
+function hello() {
+  server(8081);
+  server(8083);
+  server(8088);
 }
