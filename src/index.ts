28b6d1e1a2749a1484f3b2b15e39b8bffa99ab13
wrule <1982775886@qq.com>
2024-06-26 12:29:41 +0800

    fix: bug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,16 @@
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
 
 export
 function hello() {
-  console.log('hello world!');
+  const app = express();
+  const proxyMiddleware = createProxyMiddleware({
+    target: 'http://10.10.200.21:8083',
+    changeOrigin: true,
+    cookieDomainRewrite: '',
+    headers: { Connection: 'keep-alive' },
+  });
+  app.use('/api', proxyMiddleware);
+  app.listen(9093);
+  console.log(9093, '...');
 }
